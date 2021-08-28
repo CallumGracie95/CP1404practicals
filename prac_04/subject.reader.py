4cac8eb2531bd050e1cd7218ed1b45b40f0a7eba
Callum Gracie <callum.gracie@my.jcu.edu.au>
2021-08-28 20:12:10 +1000

    Add function to display formatted data

--- prac_04/subject.reader.py
+++ prac_04/subject.reader.py
@@ -7,23 +7,28 @@ FILENAME = "subject_data.txt"
 
 
 def main():
+    "Read data and display with formatting."
     data = get_data()
-    print(data)
+    display_subject_details(data)
 
 
 def get_data():
-    """Read data from file formatted like: subject,lecturer,number of students."""
+    """Read data from text file."""
+    data = []
     input_file = open(FILENAME)
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
-        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        data.append(parts)
+
     input_file.close()
+    return data
+
+
+def display_subject_details(data):
+    "Display formatted data."
+    for piece_of_data in data:
+        print("{} is taught by {:12} and has {} students".format(*piece_of_data))
 
 
 main()

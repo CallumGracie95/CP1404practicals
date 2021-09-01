1f5b8277b4699b834eaa8dbd6e31fe8ac0cd00fe
Callum Gracie <callum.gracie@my.jcu.edu.au>
2021-09-01 11:17:43 +1000

    Add completed list exercises activity

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,4 +1,4 @@
-"List of integers and displaying output using functions and list index."
+"1. Basic list operations"
 
 numbers = []
 for i in range(5):
@@ -13,3 +13,13 @@ print(f"The smallest number is {numbers[0]}")
 print(f"The largest number is {numbers[-1]}")
 average = sum(numbers) / len(numbers)
 print(f"The average of the numbers is {average}")
+
+"2. Woefully inadequate security checker"
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+username = input("Enter username: ")
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
+
+

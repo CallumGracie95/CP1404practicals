66a4d29e8b13612b7b48080e88cc513c5c7188ae
Callum Gracie <callum.gracie@my.jcu.edu.au>
2021-09-19 19:55:16 +1000

    Add all completed activities for practical 06

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -19,30 +19,3 @@ class Guitar:
     def is_vintage(self):
         return self.get_age() >= VINTAGE_AGE
 
-
-def main():
-    print("My guitars!")
-    guitars = []
-    name = input("Name: ")
-    while name != "":
-        year = int(input("Year: "))
-        cost = float(input("Cost: $"))
-        guitar_added = Guitar(name, year, cost)
-        print(f"{guitar_added} added")
-        guitars.append(guitar_added)
-        name = input("Guitar name: ")
-
-    if guitars:
-        guitars.sort()
-        print("These are my guitars:")
-        for i, guitar in enumerate(guitars):
-            vintage_string = ""
-            if guitar.is_vintage():
-                vintage_string = " (vintage)"
-            print(
-                "Guitar {0}: {1.name:>13} ({1.year}), worth ${1.cost:10,.2f}{2}".format(i + 1, guitar, vintage_string))
-    else:
-        print("No guitars :( Quick, go and buy one!")
-
-
-main()
